45bbe4957cf97c35807b30a80b73ff9a722b6f05
fabmilano <f.bio.milano@gmail.com>
2016-01-22 12:14:49 +0000

    m

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 // All code was wrote by Tim Kipp @ TimKippTutorials.com - December 29, 2010
-
+echo "hello"
 // Your MySQL database login information
 $host = "172.17.0.22:3306"; // Your host address to your database on your server. Usually "localhost". Check with your hosting provider
 $user = "root"; // Your username you set up for this database on your server
