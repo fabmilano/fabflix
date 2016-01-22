f1b7cf56b2632b9a2e492e77f28a40761b309db5
fabmilano <f.bio.milano@gmail.com>
2016-01-22 23:57:11 +0000

    m

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
 // if(empty($_SESSION)) // if the session not yet started
 //    session_start();
 
-// if(!isset($_SESSION['username'])) { //if not yet logged in
-//    header("Location: login.php");// send to login page
-//    exit;
-// }
+if(!isset($_SESSION['username'])) { //if not yet logged in
+   header("Location: login.php");// send to login page
+   exit;
+}
 ?>
 
 
