ed0ccb1da2d8ef75f2d74307aa58d3bbaedd5e8b
fabmilano <f.bio.milano@gmail.com>
2016-01-22 16:17:03 +0000

    m

--- index.php
+++ index.php
@@ -8,6 +8,12 @@
 <body>
 
         <h1>FabFlix</h1>
+        <?php 
+        if(isset($_GET['msg'])) {
+                $msg = urldecode($_GET['msg']);
+                echo "<p class=\"info-msg\">$msg</p>";
+        }
+        ?>
         <hr>
         <hr>
 
