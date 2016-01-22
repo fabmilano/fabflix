727c991f4b084d06dc11826969c8f581d9551c21
fabmilano <f.bio.milano@gmail.com>
2016-01-22 22:56:02 +0000

    m

--- login.php
+++ login.php
@@ -1,7 +1,6 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
+
 
 
 
@@ -21,7 +20,7 @@ if(isset($_SESSION['username'])) { // if already login
 <body>
 <form action = "login_proccess.php" method = "post">
 Username: <input type="text" name="username" /><br />
-Password: <input type="password" name="password" /><br />
+Password: <input type="text" name="password" /><br />
 <input type = "submit" name="submit" value="login" />
 </form>
 </body>
