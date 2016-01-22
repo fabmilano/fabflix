b0316f42617d1ce02ce62194a19c0e2f997c31aa
fabmilano <f.bio.milano@gmail.com>
2016-01-22 23:48:40 +0000

    m

--- index.php
+++ index.php
@@ -2,16 +2,16 @@
 
 <?php
 
-include 'connect_to_database.php'; //connect the connection page
+include 'login_proccess.php'; //connect the connection page
 
 
 if(empty($_SESSION)) // if the session not yet started
    session_start();
 
-// if(!isset($_SESSION['username'])) { //if not yet logged in
-//    header("Location: login.php");// send to login page
-//    exit;
-// }
+if(!isset($_SESSION['username'])) { //if not yet logged in
+   header("Location: login.php");// send to login page
+   exit;
+}
 ?>
 
 
