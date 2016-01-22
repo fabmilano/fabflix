76fcd07984d57e26a5489316776baf386b520af5
fabmilano <f.bio.milano@gmail.com>
2016-01-22 23:31:15 +0000

    m

--- index.php
+++ index.php
@@ -1,37 +1,34 @@
-fuck PHP
-
-// <?php
 
 
+<?php
+include 'connect_to_database.php'; //connect the connection page
 
-// include 'connect_to_database.php'; //connect the connection page
+if(empty($_SESSION)) // if the session not yet started
+   session_start();
 
-// if(empty($_SESSION)) // if the session not yet started
-//    session_start();
-
-// if(!isset($_SESSION['username'])) { //if not yet logged in
-//    header("Location: login.php");// send to login page
-//    exit;
-// }
-// ?>
+if(!isset($_SESSION['username'])) { //if not yet logged in
+   header("Location: login.php");// send to login page
+   exit;
+}
+?>
 
 
 
 
 
-// <html>
+<html>
 
-// <head>
-//         <title>FabFlix</title>
-// </head>
+<head>
+        <title>FabFlix</title>
+</head>
 
 
-// <body>
+<body>
 
-//         <h1>FabFlix</h1>
+        <h1>FabFlix</h1>
 
-//         Welcome <?php echo $_SESSION['username']; ?>,
-//         <a href="logout.php">logout</a> 
+        Welcome <?php echo $_SESSION['username']; ?>,
+        <a href="logout.php">logout</a> 
 
 
 
@@ -40,46 +37,40 @@ fuck PHP
 
 
 
-// <!--         <?php 
-//         if(isset($_GET['msg'])) {
-//                 $msg = urldecode($_GET['msg']);
-//                 echo "<p class=\"info-msg\">$msg</p>";
-                
-//         }
-//         ?> -->
-//         <hr>
-//         <hr>
+
+        <hr>
+        <hr>
 
 
-//         <a href="?page=home">Home</a> |
-//         <a href="?page=video">Videos</a> |
+        <a href="?page=home">Home</a> |
+        <a href="?page=video">Videos</a> |
 
 
-//         <hr>
+        <hr>
 
 
 
-//         <?php
+        <?php
 
-//         if(isset($_GET['page'])) {
+        if(isset($_GET['page'])) {
 
-//                 $page = $_GET["page"];
-//                 $filename = "includes/" . $page . '.php';
+                $page = $_GET["page"];
+                $filename = "includes/" . $page . '.php';
 
-//                 if(file_exists($filename)) {
-//                         include $filename;
-//                 }else{
-//                         include 'includes/home.php';
-//                 }
+                if(file_exists($filename)) {
+                        include $filename;
+                }else{
+                        include 'includes/home.php';
+                }
 
-//         }else{
-//                 include 'includes/home.php';
-//         }
+        }else{
+                include 'includes/home.php';
+        }
 
 
 
 
-//         ?>
+        ?>
 
 
 
@@ -88,7 +79,7 @@ fuck PHP
 
 
 
-// </body>
+</body>
 
 
-// </html>
+</html>
