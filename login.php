e13d5c1d8e90b9cc7c977d3d905bc91fb93e1e5d
fabmilano <f.bio.milano@gmail.com>
2016-01-22 12:21:07 +0000

    m

--- login.php
+++ login.php
@@ -1,1 +1,28 @@
-luke
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+
+// All code was wrote by Tim Kipp @ TimKippTutorials.com - December 29, 2010
+echo "hello";
+
+
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<h1>Forum Tutorial Login</h1>
+<form method="post" action="login.php">
+Username: <input type="text" name="username" /><br /><br />
+Password: <input type="password" name="password" /><br /><br />
+<input type="submit" name="submit" value="Log In" />
+</form>
+</body>
+</html>
