832f7eb260ef38176e9a51ad496e2636bc2db77b
fabmilano <f.bio.milano@gmail.com>
2016-01-23 13:50:35 +0000

    m

---
+++ includes/horror.php
@@ -0,0 +1,54 @@
+<body>
+<?php
+// connect
+$m = new Mongo("db");
+
+// select a database
+$db = $m->videos;
+
+// select a collection (analogous to a relational database's table)
+$collection = $db->videos;
+
+
+
+// find everything in the collection
+$cursor = $collection->find(array('genre' => 'horror'));
+
+
+// Sort on date ascending and age descending
+$cursor->sort(array('title' => 1));
+
+
+// iterate through the results
+foreach ($cursor as $document) {
+    $title =  $document["title"];
+    $link = "rtmp://mymongotest.cloudapp.net:1935/vod2/" .  $document["rtmp"];
+    $image = "http://mymongotest.cloudapp.net:8080/" .  $document["pic"];
+
+    
+    ?><h3><?php echo $title;?></h3>
+    <div id="instructions">
+      <video class="video-js vjs-default-skin" width="640px" height="267px" 
+      controls poster="<?php echo $image ?>" preload="none" data-setup='{ "aspectRatio":"640:267", "playbackRates": [1, 1.5, 2] }'>
+      <source src="<?php echo $link ?>"   type="rtmp/mp4" />
+      </video>
+    </div><?php
+
+   echo nl2br( "Genre: " .  $document["genre"]  .  "\n" );
+   echo nl2br( "Director: " . $document["director"] . "\n" );
+   echo "Year: " . $document["year"];
+   echo '<hr>';
+
+}
+
+
+
+
+
+
+?>
+
+</body>
+
+
+</html>
