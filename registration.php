8893748fcf447fd223273d419ef92f9c4f46e31a
fabmilano <f.bio.milano@gmail.com>
2016-01-24 12:56:08 +0000

    m

--- registration.php
+++ registration.php
@@ -13,12 +13,7 @@
  $username = $_POST['username'];
  $email = $_POST['email'];
  $password = $_POST['password'];
- // $username = stripslashes($username);
- // $username = mysql_real_escape_string($username);
- // $email = stripslashes($email);
- // $email = mysql_real_escape_string($email);
- // $password = stripslashes($password);
- // $password = mysql_real_escape_string($password);
+
 
 
  $sql = "SELECT * FROM users.users_tbl WHERE email='$email'";
