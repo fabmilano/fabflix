41613b25f67e341fa47feca8424864a515e8c8ec
fabmilano <f.bio.milano@gmail.com>
2016-01-24 11:29:54 +0000

    m

---
+++ includes/video.php
@@ -0,0 +1,62 @@
+<html>
+
+<head>
+
+
+    <link href="http://vjs.zencdn.net/4.11/video-js.css" rel="stylesheet">
+    <script src="http://vjs.zencdn.net/4.11/video.js"></script>
+
+    <script src="Base64.js"></script>
+    <script src="jquery-2.1.4.js"></script>
+    <script src="getvideo.js"></script>
+
+
+</head>
+
+<body>
+
+
+
+
+
+
+<?php
+
+// connect
+$m = new Mongo("db");
+
+// select a database
+$db = $m->videos;
+
+// select a collection (analogous to a relational database's table)
+$collection = $db->videos;
+
+
+
+// find everything in the collection
+$cursor = $collection->find();
+
+// iterate through the results
+foreach ($cursor as $document) {
+    $title =  $document["title"];
+    $link = "rtmp://mymongotest.cloudapp.net:1935/vod2/" .  $document["rtmp"];
+    ?><h3><?php echo $title;?></h3>
+    <div id="instructions"><?php
+    echo '<video class="video-js vjs-default-skin" width="640px" height="267px" controls preload="none" data-setup=\'{ "aspectRatio":"640:267", "playbackRates": [1, 1.5, 2] }\'>';
+    echo "<source src=\"$link\"   type=\"rtmp/mp4\" />";
+    ?></video><?php
+    ?></div><?php
+
+   echo nl2br( "Category: " .  $document["category"]  .  "\n" );
+   echo nl2br( "Description: " . $document["description"] . "\n" );
+   echo "Year: " . $document["year"];
+   echo '<hr>';
+
+}
+
+?>
+
+</body>
+
+
+</html>
--- registration.php
+++ registration.php
@@ -20,18 +20,29 @@
  // $password = stripslashes($password);
  // $password = mysql_real_escape_string($password);
 
- $query = "INSERT into users_tbl (username, password, email) VALUES ('$username', '$password', '$email')";
- $result = mysql_query($query);
- if($result){
- echo "<div class='form'><h3>You are registered successfully.</h3><br/>Click here to <a href='login.php'>Login</a></div>";
+
+ $sql = "SELECT FROM users_tbl WHERE email='".$email"' LIMIT 1";
+ $res = mysql_query($query);
+ if($res){
+ 	$query = "INSERT into users_tbl (username, password, email) VALUES ('$username', '$password', '$email')";
+ 	echo "<div class='form'><h3>You are registered successfully.</h3><br/>Click here to <a href='login.php'>Login</a></div>";
+ }else{
+ 	echo "Email address already in use"
  }
+
+
+ // $query = "INSERT into users_tbl (username, password, email) VALUES ('$username', '$password', '$email')";
+ // $result = mysql_query($query);
+ // if($result){
+ // echo "<div class='form'><h3>You are registered successfully.</h3><br/>Click here to <a href='login.php'>Login</a></div>";
+ // }
  }else{
 ?>
 <div class="form">
 <h1>Registration</h1>
 <form name="registration" action="" method="post">
 <input type="text" name="username" placeholder="Username" required />
-<input type="email" name="email" placeholder="Email" required />
+<input type="email" name="email" placeholder="E-mail" required />
 <input type="password" name="password" placeholder="Password" required />
 <input type="submit" name="submit" value="Register" />
 </form>
