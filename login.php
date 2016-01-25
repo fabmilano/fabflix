fc2e0f9d9330fb2f74f634cfec68ee7f263b6061
fabmilano <f.bio.milano@gmail.com>
2016-01-25 17:13:16 +0000

    m

--- login.php
+++ login.php
@@ -20,6 +20,7 @@ if(isset($_SESSION['email'])) { // if already login
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link rel="stylesheet" type="text/css" href="includes/style.css">
 	<title>Welcome, please Log In</title>
 </head>
 <center>
