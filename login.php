0a1cb7ea4b18de8a11bd4753c58fb1878dc88bef
fabmilano <f.bio.milano@gmail.com>
2016-01-25 17:44:12 +0000

    m

--- login.php
+++ login.php
@@ -28,7 +28,7 @@ if(isset($_SESSION['email'])) { // if already login
 
 <h1>FabFlix Login</h1>
 <form action = "login_proccess.php" method = "post">
-E-mail:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <input type="email" name="email" /><br /><br />
+E-mail:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <input type="email" name="email" /><br /><br />
 Password:&nbsp; <input type="password" name="password" /><br /><br /><br />
 <input type = "submit" name="submit" value="login" />
 </form>
