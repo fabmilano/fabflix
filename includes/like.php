42a5100c97f0c916dcfacfbea46542a3d93caffd
fabmilano <f.bio.milano@gmail.com>
2016-01-27 18:05:43 +0000

    m

---
+++ includes/like.php
@@ -0,0 +1,20 @@
+<?php
+
+	
+	
+	$title = $_POST['id'];                      //retreiving the value sent by javascript
+	
+	$connection = new Mongo("db"); //connecting to the database
+	$db = $connection->videos;                    //the name of my database
+	$collection = $db->videos;                       //the name of my collection
+	
+	$criteria = array("title"=>$title);                   //find the document with the specific id
+	$newdata = array('$inc'=>array("likes"=>1));     //and increase the value of the "like" property by 1
+	
+	$collection->update(
+	            $criteria,
+	            $newdata,
+	            array('multiple'=>true, 'safe'=>true)
+	          );
+	
+	?>
