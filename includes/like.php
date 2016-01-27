eeb75b0ce94f1853c3a5cc2c211dff853adcd96b
fabmilano <f.bio.milano@gmail.com>
2016-01-27 19:11:17 +0000

    m

--- includes/like.php
+++ includes/like.php
@@ -1,21 +1,20 @@
 <?php
-	error_reporting(E_ALL);
-	ini_set('display_errors', 1);
+
 	
-	print_r($_POST); 
-	// $title = $_POST['value'];                      //retreiving the value sent by javascript
 	
-	// $connection = new Mongo("db"); //connecting to the database
-	// $db = $connection->videos;                    //the name of my database
-	// $collection = $db->videos;                       //the name of my collection
+	$title = strval($_POST['title']);                      //retreiving the value sent by javascript
 	
-	// $criteria = array("title"=>$title);                   //find the document with the specific id
-	// $newdata = array('$inc'=>array("likes"=>1));     //and increase the value of the "like" property by 1
+	$connection = new Mongo("db"); //connecting to the database
+	$db = $connection->CATALOGUE;                    //the name of my database
+	$collection = $db->movies;                       //the name of my collection
 	
-	// $collection->update(
-	//             $criteria,
-	//             $newdata,
-	//             array('multiple'=>true, 'safe'=>true)
-	//           );
+	$criteria = array("title"=>$title);                   //find the document with the specific id
+	$newdata = array('$inc'=>array("likes"=>1));     //and increase the value of the "like" property by 1
+	
+	$collection->update(
+	            $criteria,
+	            $newdata,
+	            array('multiple'=>true, 'safe'=>true)
+	          );
 	
 	?>
